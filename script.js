67eb9a9ed84ae2deefd1e4a2e26dae313aab2d4a
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-05-21 20:40:15 -0300

    validacaoTest1

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const botaoLogar = document.getElementById("logar")
 botaoLogar.addEventListener("click",() => {
     window.location.href = "index.html"
 })
+
+var inputEmail = document.querySelector(".input__login")
+var botaoEnviar = document.querySelector(".botao__entrar-login-direito")
+
+botaoEnviar.addEventListener("click", () => {
+    alert(inputEmail)
+})
