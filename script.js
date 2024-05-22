922aa60284182b4f1e446e1404053fabda19e92e
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-05-21 21:45:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 
-// Seleciona o elemento que exibirá mensagens de erro
+
 var texto = document.querySelector(".mensagem-erro");
 
-// Seleciona o botão de enviar do formulário de login
+
 var botaoEnviar = document.querySelector(".botao__entrar-login-direito");
 
-// Adiciona um listener para o evento de clique no botão de enviar
+
 botaoEnviar.addEventListener("click", (event) => {
-    // Obtém o valor do campo de email
+    
     var inputEmail = document.querySelector(".input__login").value;
 
-    // Verifica se o valor do campo de email contém "@" e "." 
+
     if (inputEmail.includes("@") && inputEmail.includes(".", inputEmail.indexOf("@"))) {
-        // Se o formato do email estiver válido, você pode realizar outras ações aqui
-        // Por exemplo, você pode enviar o formulário ou fazer algum outro processamento
-        alert("O email é válido");
         texto.classList.add("hide");
+        alert("O email é válido");
+        
     } else {
-        // Se o formato do email estiver inválido, exibe uma mensagem de erro
         texto.classList.remove("hide");
         texto.innerHTML = "E-mail inválido";
         event.preventDefault();
