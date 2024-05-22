fe65d3ee820e1d2bcc7f067662c3a620680c2d0b
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-05-21 21:36:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const botaoLogar = document.getElementById("logar")
+const botaoLogar = document.getElementById("logar");
 
-botaoLogar.addEventListener("click",() => {
-    window.location.href = "index.html"
-})
+
+botaoLogar.addEventListener("click", () => {
+
+    window.location.href = "index.html";
+});
+
+
 var texto = document.querySelector(".mensagem-erro");
 
-var botaoEnviar = document.querySelector(".botao__entrar-login-direito")
+
+var botaoEnviar = document.querySelector(".botao__entrar-login-direito");
+
 
 botaoEnviar.addEventListener("click", () => {
-    var inputEmail = document.querySelector(".input__login").value
-    alert("sasad")
-    if( (inputEmail.search("@") >= 0 ) && (inputEmail.indexOf(".", inputEmail.search("@"))) && (inputEmail.search(" " == -1)){
-        alert("boa")
-    }
+    
+    var inputEmail = document.querySelector(".input__login").value;
+
+
+    if (inputEmail.includes("@") && inputEmail.includes(".", inputEmail.indexOf("@"))) {
+        alert("O email é válido");
+    } else {
         
-    else {
         texto.classList.remove("hide");
-        texto.innerHTML = "E-mail invalido";
+        texto.innerHTML = "E-mail inválido";
         return;
     }
-        
-})
-
+});
