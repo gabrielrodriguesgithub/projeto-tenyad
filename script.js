d1411067952d0fc03038d13c4403c0bf3aaeb9d8
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-05-21 21:08:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const botaoLogar = document.getElementById("logar")
 botaoLogar.addEventListener("click",() => {
     window.location.href = "index.html"
 })
-
+var texto = document.querySelector(".mensagem-erro");
 
 var botaoEnviar = document.querySelector(".botao__entrar-login-direito")
 
 botaoEnviar.addEventListener("click", () => {
     var inputEmail = document.querySelector(".input__login").value
-    
+    if(inputEmail.search("@") >= 0) {
+        if(inputEmail.indexOf(".", inputEmail.search("@")) == 1) {
+            alert("foi")
+        }
+    }else {
+        texto.classList.remove("hide");
+        texto.innerHTML = "E-mail invalido";
+    }
+        
 })
 
