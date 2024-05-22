6af951ca691316f1ae889f09adc1aac2fb476126
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2024-05-21 21:34:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,9 @@ var botaoEnviar = document.querySelector(".botao__entrar-login-direito")
 botaoEnviar.addEventListener("click", () => {
     var inputEmail = document.querySelector(".input__login").value
     alert("sasad")
-    if(inputEmail.search("@") >= 0 ) &&
-    (inputEmail.indexOf(".", inputEmail.search("@"))) 
-    //(inputEmail.search(" " == -1)) 
-    //{
-        )}
-    //}
+    if( (inputEmail.search("@") >= 0 ) && (inputEmail.indexOf(".", inputEmail.search("@"))) && (inputEmail.search(" " == -1)){
+        alert("boa")
+    }
         
     else {
         texto.classList.remove("hide");
